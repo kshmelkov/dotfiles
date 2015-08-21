59cc5684751ce78ebfd28212e4fbfecb3e659349
Konstantin Shmelkov <konstantin@shmelkov.net>
2015-08-21 22:28:08 +0300

    migration to st

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -43,7 +43,7 @@ end
 beautiful.init("~/.config/awesome/theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "st"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
